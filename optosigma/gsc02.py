001aa68b3ff4383902ab69649441ffce3d0d310e
Ryota MAEDA <maenosuke079@gmail.com>
2022-12-20 18:10:55 +0900

    Apply black formatting

--- optosigma/gsc02.py
+++ optosigma/gsc02.py
@@ -6,14 +6,15 @@ import serial
 
 _gsc02_opened_objects = dict()
 
+
 class GSC02(serial.Serial):
     """Wrapper for GSC-02 controller"""
 
     def open(self):
-        """Open port with current settings. 
+        """Open port with current settings.
         This may throw a SerialException if the port cannot be opened.
-        
-        The difference with the original `serial.Serial` class is 
+
+        The difference with the original `serial.Serial` class is
         that it *only* executed when opening a *new* port.
         """
         if self.port not in _gsc02_opened_objects:
@@ -24,7 +25,7 @@ class GSC02(serial.Serial):
     def close(self):
         """Close port"""
         super().close()
-        
+
         if self.port in _gsc02_opened_objects:
             del _gsc02_opened_objects[self.port]
 
@@ -35,9 +36,9 @@ class GSC02(serial.Serial):
         # If opened object does not exist,
         if self.port not in _gsc02_opened_objects:
             return super().__getattribute__(name)
-        
+
         opened_object = _gsc02_opened_objects[self.port]
-        
+
         # If this object is equal to opened object,
         if id(self) == id(opened_object):
             return super().__getattribute__(name)
@@ -54,7 +55,7 @@ class GSC02(serial.Serial):
             is_overridden = this_attr.__func__ != opened_attr.__func__
         else:
             is_overridden = this_attr != opened_attr
-        
+
         # Rturn overridden attribute
         if is_overridden:
             return super().__getattribute__(name)
@@ -68,7 +69,7 @@ class GSC02(serial.Serial):
         ----------
         cmd : str
             Command
-            
+
         Returns
         -------
         ret : bool or Any
@@ -83,7 +84,7 @@ class GSC02(serial.Serial):
             return self.readline().decode()[:-2]  # -2: Remove terminator characters (CR + LF)        
         else:
             return self.is_last_command_success
-    
+
     def _get_position(self, axis: Union[int, str]) -> int:
         if axis in [1, "1"]:
             return self.get_status1()[0]
@@ -102,12 +103,12 @@ class GSC02(serial.Serial):
     def position1(self) -> int:
         """Current stage position of axis 1"""
         return self._get_position(axis=1)
-    
+
     @position1.setter
     def position1(self, target_position):
         """Move stage to target position of axis 1"""
         self._set_position(target_position, axis=1)
-    
+
     @property
     def position2(self) -> int:
         """Current stage position of axis 2"""
@@ -140,7 +141,7 @@ class GSC02(serial.Serial):
     @property
     def ack3(self) -> str:
         """ACK3
-        
+
         B  Busy status
         R  Ready status
         """
@@ -172,9 +173,9 @@ class GSC02(serial.Serial):
     def return_origin(self, direction: Union[str, Sequence[str]], axis: Union[int, str]) -> bool:
         """H command: Return to mechanical origin
 
-        This command is used to detect the mechanical origin for a stage and set that position as the origin. 
-        Once the mechanical origin has been detected, the value displayed will be 0. 
-        Each axis moves at the following constant conditions: Minimum speed (S): 500PPS, Maximum speed (F): 5000PPS, Acceleration/ Deceleration time (R): 200mS. 
+        This command is used to detect the mechanical origin for a stage and set that position as the origin.
+        Once the mechanical origin has been detected, the value displayed will be 0.
+        Each axis moves at the following constant conditions: Minimum speed (S): 500PPS, Maximum speed (F): 5000PPS, Acceleration/ Deceleration time (R): 200mS.
         Axes to home are depending on the DIP Switch settings.
 
         Parameters
@@ -225,12 +226,12 @@ class GSC02(serial.Serial):
         else:
             msg = f'"{axis}" is not supported, choose axis from [1, 2, "W"]'
             raise ValueError(msg)
-        
+
     def set_relative_pulse(self, pulse: Union[int, Sequence[int]], axis: Union[int, str]) -> bool:
         """M command: Set number of pulses for relative travel
 
-        This command is to specify the axis of travel, direction, and the travel (number of pulses). 
-        This command must always be followed by a drive (G) command. 
+        This command is to specify the axis of travel, direction, and the travel (number of pulses).
+        This command must always be followed by a drive (G) command.
         Travel is by means of acceleration/deceleration driving.
 
         Parameters
@@ -273,8 +274,8 @@ class GSC02(serial.Serial):
     def jog(self, direction: Union[str, Sequence[str]], axis: Union[int, str]) -> bool:
         """J command: JOG
 
-        This command drives stages continuously (at a constant speed) at the starting speed (S). 
-        This command must always be followed by a drive (G) command. 
+        This command drives stages continuously (at a constant speed) at the starting speed (S).
+        This command must always be followed by a drive (G) command.
         The stage will stop by an L command.
 
         Parameters
@@ -323,7 +324,7 @@ class GSC02(serial.Serial):
     def driving(self) -> bool:
         """G command: Drive
 
-        When a drive command is issued, the stage starts moving, moves the specified number of pulses, and then stops. 
+        When a drive command is issued, the stage starts moving, moves the specified number of pulses, and then stops.
         The G command is used after M and J commands.
 
         Returns
@@ -338,7 +339,7 @@ class GSC02(serial.Serial):
 
         When this command is executed, the stage decelerates and stops.
 
-        Stage does not stop even if“ L:1”,“ L:2” at the time“ H:”. 
+        Stage does not stop even if“ L:1”,“ L:2” at the time“ H:”.
         Stop in“ L:W" or“ L:E".
 
         Parameters
@@ -369,7 +370,7 @@ class GSC02(serial.Serial):
         """R command: Set electronic (logical) origin
 
         This command is used to set electronic (logical) origin to the current position of each axis.
-        
+
         Parameters
         ----------
         axis : Union[int, str]
@@ -385,14 +386,14 @@ class GSC02(serial.Serial):
     def set_speed(self, spd_range: int, spd_min: Sequence[int], spd_max: Sequence[int], acceleration_time: Sequence[int]) -> bool:
         """D command: Speed settings
 
-        The minimum speed (S), maximum speed (F), and acceleration/deceleration time (R) are set according to the initialize settings when the power is turned on. 
-        This command allows you to change these initial settings. 
+        The minimum speed (S), maximum speed (F), and acceleration/deceleration time (R) are set according to the initialize settings when the power is turned on.
+        This command allows you to change these initial settings.
         The initialize setting is (S): 500PPS, (F): 5000PPS, (R): 200mS.
 
-        The maximum speed(F) setting should be equal or greater than the minimum speed. 
-        If the minimum speed is set to equals to the maximum or the acceleration/deceleration time is set to zero, 
+        The maximum speed(F) setting should be equal or greater than the minimum speed.
+        If the minimum speed is set to equals to the maximum or the acceleration/deceleration time is set to zero,
         stages will move at a constant speed without performing acceleration/deceleration logically.
-        
+
         Parameters
         ----------
         spd_rande : int
@@ -421,7 +422,7 @@ class GSC02(serial.Serial):
         return self.raw_command(f"D:{r}S{spd11}F{spd21}R{spd31}S{spd12}F{spd22}R{spd32}")
 
     def energize_motor(self, energize: Union[bool, Sequence[bool]], axis: Union[int, str]) -> bool:
-        """ C command: Free/ hold motor (Excitation ON/OFF)
+        """C command: Free/ hold motor (Excitation ON/OFF)
 
         This command is used to excite the motor or to turn excitation off, making it possible to move (rotate) stages manually.
 
@@ -437,7 +438,7 @@ class GSC02(serial.Serial):
         -------
         ret : bool
             Whether the command is a success or not.
-        
+
         Examples
         --------
         >>> energize_motor(False, axis=1)
@@ -458,29 +459,29 @@ class GSC02(serial.Serial):
         else:
             msg = f'"{axis}" is not supported, choose axis from [1, 2, "W"]'
             raise ValueError(msg)
-        
+
     def get_status1(self) -> Tuple[int, int, str, str, str]:
         """Q command: Status 1
 
-        On receipt of this command, the controller returns the coordinates for each axis and the current state of each stage.
+         On receipt of this command, the controller returns the coordinates for each axis and the current state of each stage.
 
-        Returns
-        -------
-        position1 : int
-            current position of axis 1
-        position2 : int
-            current position of axis 2
-        ack1 : str
-            X  Command error
-            K  Command accepted normaly
-        ack2 : str
-            L  First axis stopped at LS
-            M  Second axis stopped at LS
-            W  First and second axes stopped at LS
-            K  Normal stop
-       ack3 : str
-            B  Busy status
-            R  Ready status
+         Returns
+         -------
+         position1 : int
+             current position of axis 1
+         position2 : int
+             current position of axis 2
+         ack1 : str
+             X  Command error
+             K  Command accepted normaly
+         ack2 : str
+             L  First axis stopped at LS
+             M  Second axis stopped at LS
+             W  First and second axes stopped at LS
+             K  Normal stop
+        ack3 : str
+             B  Busy status
+             R  Ready status
         """
         status = self.raw_command("Q:").split(",")
         position1 = int(status[0].replace(" ", ""))
@@ -492,7 +493,7 @@ class GSC02(serial.Serial):
 
     def get_status2(self) -> str:
         """! command: Status2
-        
+
         On receipt of this command, the controller returns the stage operating status.
 
         Returns
